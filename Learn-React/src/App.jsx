6cdad3473400b97b4187dbf0ecfc798fbd155cd2
Abhishek Raj <abhishekr359@gmail.com>
2024-07-14 21:04:08 +0530

    add a logic when count > 0 never go in negative and when count is < 20 never goes up

--- Learn-React/src/App.jsx
+++ Learn-React/src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 
 function App() {
   let [count, setCount] = useState(0); // useState returns an array with two elements, the first element is the state variable and the second element is the function to update the state variable. use to propogate the changes to the UI.
-  console.log("count", count);
+  console.log(`count: ${count}`);
+
   return (
     <>
       <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment: {count}</button>
+      <button onClick={() => {if(count < 20) setCount(count += 1)}}>Increment: {count}</button>
       <br /> <br />
-      <button onClick={() => setCount(count - 1)}>Decrement: {count}</button>
+      <button onClick={() => {if(count > 0) setCount(count -= 1)}}>Decrement: {count}</button>
     </>
   );
 }
