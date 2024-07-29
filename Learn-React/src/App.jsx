e18e3c2c00c9c04a753cafbfee9b2546ee513b19
unknown <abhishekr359@gmail.com>
2024-07-29 18:35:55 +0530

    changing the style of the bg-changer

--- Learn-React/src/App.jsx
+++ Learn-React/src/App.jsx
@@ -6,20 +6,16 @@ function App() {
   let [count, setCount] = useState(0); // useState returns an array with two elements, the first element is the state variable and the second element is the function to update the state variable. use to propogate the changes to the UI.
   console.log(`count: ${count}`);
 
-  const increaseCount = () =>{
-    if(count < 20) {
-      setCount(count + 1)
+  const increaseCount = () => {
+    if (count < 20) {
+      setCount(count + 1);
     }
-  }
-  
+  };
+
   return (
     <>
       <h1 className="bg-green-500 rounded-xl m-4">Counter: {count}</h1>
-      <button
-        onClick={increaseCount}
-      >
-        Increment: {count}
-      </button>
+      <button onClick={increaseCount}>Increment: {count}</button>
       <br /> <br />
       <button
         className="m-4"
@@ -29,8 +25,8 @@ function App() {
       >
         Decrement: {count}
       </button>
-      <Card name ='Abhishek' data = 'This is demo paragraph 1' />
-      <Card name= 'Aman'data= 'This is demo paragraph 2' />
+      <Card name="Abhishek" data="This is demo paragraph 1" />
+      <Card name="Aman" data="This is demo paragraph 2" />
     </>
   );
 }
--- Learn-React/src/pages/BgChanger.jsx
+++ Learn-React/src/pages/BgChanger.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 function BgChanger() {
-    const [colour , setColour] = useState('white')
+  const [colour, setColour] = useState("white");
+
+  useEffect(() => {
+    document.body.style.backgroundColor = colour;
+  }, [colour]);
 
   return (
     <>
-    <div className="h-screen w-screen" style={{backgroundColor : colour}}>
-    <div className="pt-4 pl-2 ">
-    <button onClick={() => setColour("red")} className="mr-3 bg-red-600 rounded-full">Red</button>
-    <button onClick={() => setColour("blue")} className="mr-3 bg-blue-600 rounded-full">Blue</button>
-    <button onClick={() => setColour("green")} className="mr-3 bg-green-600 rounded-full">Green</button>
-    <button onClick={() => setColour("black")} className="mr-3 bg-black rounded-full">Black</button>
-    </div>
-    </div>
+      <header className="flex justify-start">
+        <button
+          onClick={() => setColour("red")}
+          className="mr-3 bg-red-600 rounded-full"
+        >
+          Red
+        </button>
+        <button
+          onClick={() => setColour("blue")}
+          className="mr-3 bg-blue-600 rounded-full"
+        >
+          Blue
+        </button>
+        <button
+          onClick={() => setColour("green")}
+          className="mr-3 bg-green-600 rounded-full"
+        >
+          Green
+        </button>
+        <button
+          onClick={() => setColour("black")}
+          className="mr-3 bg-black rounded-full"
+        >
+          Black
+        </button>
+      </header>
     </>
-  )
+  );
 }
 
-export default BgChanger
+export default BgChanger;
